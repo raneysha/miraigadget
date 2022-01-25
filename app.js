a99ef70bede084ab6ad411d2918c2513207fd6df
raneysha <cahyo123dwi@gmail.com>
2022-01-25 12:35:37 +0700

    aoa

---
+++ app.js
@@ -0,0 +1,26 @@
+readJson = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const name = urlParams.get('name')
+    // http://localhost:8080
+    fetch('https://raneysha.github.io/miraigadget/source.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error " + response.status);
+            }
+            return response.json();
+        })
+        .then(json => {
+            //    this.users = json;
+            //    console.log(this.users);
+            json.forEach(element => {
+                if (element.name == name) {
+                    window.location.replace(element.link);
+                }
+            });
+        })
+        .catch(function () {
+            this.dataError = true;
+        })
+}
+readJson();
